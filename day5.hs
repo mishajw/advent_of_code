11f46e89a9aeebfb33d323afdcc1ec3bc33a0330
Misha Wagner <mishajw@gmail.com>
2017-12-05 13:45:39 +0000

    Add solution for day 5 part 2

--- day5.hs
+++ day5.hs
@@ -1,16 +1,26 @@
+type IncrementRule = Int -> Int
+
 main :: IO ()
 main = do
   input <- getFile "inputs/day5.txt"
-  let steps = getSteps input
-  putStrLn $ "Steps: " ++ show steps
+  let firstSteps = getSteps firstIncrementRule input
+  let secondSteps = getSteps secondIncrementRule input
+  putStrLn $ "First steps: " ++ show firstSteps
+  putStrLn $ "Second steps: " ++ show secondSteps
 
 getFile :: String -> IO [Int]
 getFile path = do
   input <- readFile path
   return $ map read . lines $ input
 
-getSteps :: [Int] -> Int
-getSteps instructions = getSteps' 0 0 instructions
+firstIncrementRule :: IncrementRule
+firstIncrementRule = (+1)
+
+secondIncrementRule :: IncrementRule
+secondIncrementRule x = if x > 3 then x - 1 else x + 1
+
+getSteps :: IncrementRule -> [Int] -> Int
+getSteps rule instructions = getSteps' 0 0 instructions
   where
     getSteps' :: Int -> Int -> [Int] -> Int
     getSteps' step index instructions =
@@ -24,5 +34,5 @@ getSteps instructions = getSteps' 0 0 instructions
 
     incrementInstruction :: Int -> [Int] -> [Int]
     incrementInstruction index instructions =
-      take index instructions ++ (instructions !! index) + 1 : drop (index + 1) instructions
+      take index instructions ++ rule (instructions !! index) : drop (index + 1) instructions
 
